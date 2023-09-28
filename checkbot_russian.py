0f6dc17561b74cb0f796bb41cc268f04269883e5
Fakhrillo <dombit1999@gmail.com>
2023-09-28 11:46:29 +0500

    typ

--- checkbot_russian.py
+++ checkbot_russian.py
@@ -251,7 +251,7 @@ def handle_photo(message):
     else:
         print(f"Error getting token: {response.status_code}")
 
-    if user_id == worker_data['tg_id'] and worker_data:
+    if worker_data and user_id == worker_data['tg_id']:
 
         # Retrieve the user's current step from Redis
         user_step_bytes = redis_client.get(f'user_step:{user_id}')
