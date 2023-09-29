487dd2413a9bf5f111a8da1322e3129a07b4991b
Fakhrillo <dombit1999@gmail.com>
2023-09-29 18:43:08 +0500

    requestwithpone

--- checkbot_russian.py
+++ checkbot_russian.py
@@ -120,7 +120,7 @@ def handle_contact(message):
             worker_data = response.json()
             tg_id_update = {'id_tg': user_id,}
             print(f'DATA: {tg_id_update}')
-            response_update = requests.patch(f'{API_URL}worker/{worker_data["id"]}', data=tg_id_update, headers=headers)
+            response_update = requests.patch(f'{API_URL}worker/{user_phone}', data=tg_id_update, headers=headers)
             print(f"Response code: {response_update}")
             verification_code = ''.join(random.choice('0123456789') for i in range(6))
             verification_codes[user_id] = verification_code
